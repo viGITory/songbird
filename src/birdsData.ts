851d86ced350c3df0efaec66d774993a9481eb52
PANTORY <pantory@gmail.com>
2022-11-29 20:59:12 +0300

    fix: broken image link

--- src/birdsData.ts
+++ src/birdsData.ts
@@ -358,7 +358,7 @@ const birdsData = [
         species: 'Pelecanus',
         description:
           'Пеликаны — обитатели морей и рек. Ходят они неуклюже, но хорошо летают и плавают. Питаются в основном рыбой, устраивают коллективные охоты — выстроившись полукругом хлопают по воде крыльями и клювами и вытесняют напуганную рыбу на мелководье.',
-        image: 'https://live.staticflickr.com/65535/49159147156_dcbbb5c12a.jpg',
+        image: 'https://live.staticflickr.com/1444/26348821571_e9783dd6cd_b.jpg',
         audio: 'https://www.xeno-canto.org/sounds/uploaded/XAMHIHFTZG/XC331138-call1.mp3',
       },
       {
